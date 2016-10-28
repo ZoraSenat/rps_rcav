2b68845fa236aba51c7f5cd1bb628ca635df4c4d
zora <ssh>
2016-10-27 21:38:26 -0500

    Consolidated methods to 1

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,17 @@
 class GameController < ApplicationController
   #exception to capital letters (first letter of class must be capital letter)
-  def user_plays_rock
-    @user_move = "rock"
+
+  def user_plays_anything
+    @user_move = params["move"]
+
     @computer_move = ["rock","paper","scissors"].sample
 
-    if @computer_move == "rock"
+    if @computer_move == @user_move
       @result = "You tied!"
     else
-      if @computer_move == "scissors"
+      if ((@computer_move == "scissors" && @user_move == "rock") ||
+        (@computer_move == "paper" && @user_move == "scissors") ||
+        (@computer_move == "rock" && @user_move == "paper"))
         @result = "You won!"
       else
         @result = "You lost!"
@@ -16,36 +20,4 @@ class GameController < ApplicationController
     render("game/play_anything.html.erb")
   end
 
-
-  def user_plays_paper
-    @user_move = "paper"
-    @computer_move = ["rock","paper","scissors"].sample
-
-    if @computer_move == "paper"
-      @result = "You tied!"
-    else
-      if @computer_move == "rock"
-        @result = "You won!"
-      else
-        @result = "You lost!"
-      end
-    end
-    render("game/play_anything.html.erb")
-  end
-
-  def user_plays_scissors
-    @user_move = "scissors"
-    @computer_move = ["rock","paper","scissors"].sample
-
-    if @computer_move == "scissors"
-      @result = "You tied!"
-    else
-      if @computer_move == "paper"
-        @result = "You won!"
-      else
-        @result = "You lost!"
-      end
-    end
-    render("game/play_anything.html.erb")
-  end
 end
